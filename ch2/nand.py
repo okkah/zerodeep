51b244b788619d545b4bda4ad95659fcc0d0d8dd
okkah <trombonehide@yahoo.co.jp>
2017-11-30 19:49:55 +0900

    change ch2

--- ch2/nand.py
+++ ch2/nand.py
@@ -1,11 +1,11 @@
 #NANDゲートをパーセプトロンで実装
 
 def NAND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
+    w1, w2, theta = -0.5, -0.5, -0.7
     tmp = x1*w1 + x2*w2
-    if tmp >= theta:
+    if tmp <= theta:
         return 0
-    if tmp < theta:
+    if tmp > theta:
         return 1
 
 print(NAND(0, 0)) #1を出力
