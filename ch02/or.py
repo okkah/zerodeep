2a5e0543fa256371fb3510484698270221dd10d6
okkah <trombonehide@yahoo.co.jp>
2017-12-04 20:41:34 +0900

    initial commit

--- ch02/or.py
+++ ch02/or.py
@@ -1,12 +1,12 @@
 #ORゲートをパーセプトロンで実装
 
 def OR(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.5
+    w1, w2, theta = 0.5, 0.5, 0.2
     tmp = x1*w1 + x2*w2
-    if tmp == theta:
-        return 1
-    else:
+    if tmp <= theta:
         return 0
+    elif tmp > theta:
+        return 1
 
 print(OR(0, 0)) #0を出力
 print(OR(1, 0)) #1を出力
